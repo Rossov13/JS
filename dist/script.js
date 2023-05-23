f90906460f323e979fc4ea24c6ae08e6218b4679
Neal Rossov <nealrossov@gmail.com>
2023-05-23 20:43:01 +0300

    homework

--- dist/script.js
+++ dist/script.js
@@ -93,30 +93,173 @@
 //   numbDig ++
 // }
 // alert (numbDig)   
-var numberA = 0;
-var numberB = 0;
-var result = true;
-var action_sign = "";
-do {
-    numberA = +prompt("Write the first number: ");
-    numberB = +prompt("Write the second numbeк: ");
-    action_sign = prompt("Write the sign (*, /, + либо -): ");
-    switch (action_sign) {
-        case "+":
-            alert(numberA + " " + action_sign + " " + numberB + " = " + (numberA + numberB));
-            break;
-        case "-":
-            alert(numberA + " " + action_sign + " " + numberB + " = " + (numberA - numberB));
-            break;
-        case "*":
-            alert(numberA + " " + action_sign + " " + numberB + " = " + numberA * numberB);
-            break;
-        case "/":
-            alert(numberA + " " + action_sign + " " + numberB + " = " + numberA / numberB);
-            break;
-        default:
-            alert("\u0412\u044B \u043D\u0435 \u0432\u0432\u0435\u043B\u0438 \u0437\u043D\u0430\u043A \u043C\u0430\u0442\u0435\u043C\u0430\u0442\u0438\u0447\u0435\u0441\u043A\u043E\u0439 \u043E\u043F\u0435\u0440\u0430\u0446\u0438\u0438!");
-            break;
-    }
-    result = confirm("Хотите выполнить еще одну математическую операцию?");
-} while (result == true);
+// let numberA = 0;
+// let numberB = 0;
+// let result = true;
+// let action_sign = "";
+// do {
+//   numberA = +(prompt("Write the first number: ") as string);
+//   numberB = +(prompt("Write the second numbeк: ") as string);
+//   action_sign = (prompt("Write the sign (*, /, + либо -): ")as string);
+//   switch (action_sign) {
+//     case "+":
+//       alert(
+//         `${numberA} ${action_sign} ${numberB} = ${numberA + numberB}`
+//       );
+//       break;
+//     case "-":
+//       alert(
+//         `${numberA} ${action_sign} ${numberB} = ${numberA - numberB}`
+//       );
+//       break;
+//     case "*":
+//       alert(
+//         `${numberA} ${action_sign} ${numberB} = ${numberA * numberB}`
+//       );
+//       break;
+//     case "/":
+//       alert(
+//         `${numberA} ${action_sign} ${numberB} = ${numberA / numberB}`
+//       );
+//       break;
+//     default:
+//       alert(`Вы не ввели знак математической операции!`);
+//       break;
+//   }
+//   result = confirm("Хотите выполнить еще одну математическую операцию?");
+// } while (result == true);
+// let base_number = Math.abs(
+//     Math.trunc(+(prompt("Write the number for transformation: ")as string))
+//   );
+//   let jump = Math.abs(
+//     Math.trunc(
+//       +(prompt("Write how far it should be moved: ") as string)
+//     )
+//   );
+//   let array = [];
+//   let new_base_number = base_number;
+//   while (base_number > 0) {
+//     array.unshift(base_number % 10);
+//     base_number = Math.trunc(base_number / 10);
+//   } 
+//   let new_jump = jump;
+//   jump = jump % array.length; 
+//   let arrJumpLeft_L =  [];
+//   let arrJumpLeft_R =  [];
+//   let arrJumpRight_L = [];
+//   let arrJumpRight_R = [];
+//   for (let i = 0; i < array.length; i++) {
+//     if (i < jump) {
+//       arrJumpLeft_L[i] = array[i];
+//     } else {
+//       arrJumpLeft_R[i] = array[i];
+//     }
+//     if (i < array.length - jump) {
+//       arrJumpRight_L[i] = array[i];
+//     } else {
+//       arrJumpRight_R[i] = array[i];
+//     }
+//   }
+//   alert(
+//     `"${new_base_number}" ${new_jump}` +
+//       arrJumpRight_R.concat(arrJumpRight_L).join("")
+//   ); 
+//   alert(
+//     ` "${new_base_number}" ${new_jump} ` +
+//       arrJumpLeft_R.concat(arrJumpLeft_L).join("")
+//   )
+// let N = 0;
+// do{
+//     N = Math.trunc(
+//         +prompt('write the number in the range from 0 to 100') 
+//     );
+// } while (N < 0 || N > 0);
+// let counter = 0; 
+// let low = 0; 
+// let middle = 0; 
+// let high = 100; 
+// let condition = "";
+// while (low <= high) {
+//     middle= Math.trunc((low + high) / 2);
+//     condition = prompt(
+//         `Have you guessed the number ${middle}? please write without quotation "=" if it is right. In other way please write withput quotation "<" if it is less or ">" if it is more!`
+//     );
+//     if (condition != "=" && condition != "<" && condition != ">") {
+//         alert(
+//           "You are writing non-valid numbers"
+//         );
+//         continue;
+// }
+// if (
+//     (condition != "=" && middle == N) ||
+//     (condition == "<" && middle < N) ||
+//     (condition == ">" && middle > N) ||
+//     (condition == "=" && middle != N)
+//   ) {
+//     alert(
+//       `Sign "${condition}" is invalid! Try again!`
+//     );
+//     continue;
+//   }
+//   if (condition == ">") {
+//     low = middle + 1;
+//   } else if (condition == "<") {
+//     high = middle - 1;
+//   } else {
+//     low = high + 1
+//   }
+//   counter++;
+// }
+// alert(`So, for ${counter} attempts we've got, that your number is ${middle}!`
+// );
+// let N = 0;
+//   do {
+//     N = Math.trunc(
+//       +prompt("Введите целое число в диапазоне от 0 до 100 включительно: ")
+//     );
+//   } while (N < 0 || N > 100);
+//   alert(`Итак, будем играться с числом ${N}.`);
+//   let counter = 0; 
+//   let low = 0; 
+//   let middle = 0; 
+//   let high = 100; 
+//   let condition = ""; 
+//   while (low <= high) {
+//     middle = Math.trunc((low + high) / 2);
+//     condition = prompt(
+//       `Вы загадали число ${middle}? Введите без кавычек "=", если это так. В противном случае, если искомое число меньше, введите "<", либо же ">", если оно больше. Опять же, без кавычек.`
+//     );
+//     if (condition == ">") {
+//       low = middle + 1;
+//     } else if (condition == "<") {
+//       high = middle - 1;
+//     } else {
+//       low = high + 1;
+//     }
+//     counter++;
+//   }
+//   alert(
+//     `Итак, за ${counter} попыток бинарным поиском мы установили, что вы загадали число ${middle}!`
+//   );
+// }
+// let n = 0;
+// let positive_number = 0;
+// let negative_number = 0;
+// let null_number = 0;
+// let even_number = 0;
+// let odd_number = 0;
+// for (let i = 0; i < 10; i++) {
+//   n = +prompt("Введите число: ");
+//   if (n > 0) {
+//     positive_number += 1;
+//   } else if (n < 0) {
+//     negative_number += 1;
+//   } else {
+//     null_number += 1;
+//   }
+//   !(n % 2) ? (even_number += 1) : (odd_number += 1);
+// }
+// alert(
+//   `Из 10 введеных вами чисел ${positive_number} положительных, ${negative_number} отрицательных, ${null_number} равных нулю, ${even_number} четных и ${odd_number} нечетных.`
+// );
+// }
